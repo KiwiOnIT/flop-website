0a951c9c8cb833a89464ae6cd850a529c99203a9
KiwiIT <77931084+KiwiOnIT@users.noreply.github.com>
2022-05-07 22:01:25 +0200

    Update TheFooter.vue

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="text-center p-3" style="background-color: rgba(33, 37, 41, 1);">
    ©2022     Made with ❤️ by
-        <a href="https://github.com/KiwiOnWii">KiwiIT</a>
+        <a href="https://github.com/KiwiOnIT">KiwiIT</a>
 </div>
 </template>
